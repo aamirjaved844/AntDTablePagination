d074d328988e9c0bd487028efe37ae2009a7aef9
Aamir Javed <aamirjaved844@gmail.com>
2022-01-22 13:44:06 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AntDTablePagination
+This repo contains code base regarding youtube tutorial (https://youtu.be/AXNp_oU1Q80) on how to implement Table pagination with data fetched from server using axios.
+
+Notes: If somehow due to difference in react versions or so, it is not compiled correctly then you can use the code from App.js file into your project directly.
---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./App.css";
+import { Table } from "antd";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [dataSource, setDataSource] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchRecords(1);
+  }, []);
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Trips",
+      dataIndex: "trips",
+    },
+  ];
+
+  const fetchRecords = (page) => {
+    setLoading(true);
+    axios
+      .get(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=10`)
+      .then((res) => {
+        setDataSource(res.data.data);
+        setTotalPages(res.data.totalPages);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Table
+        loading={loading}
+        columns={columns}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 10,
+          total: totalPages,
+          onChange: (page) => {
+            fetchRecords(page);
+          },
+        }}
+      ></Table>
+    </div>
+  );
+}
+export default App;
